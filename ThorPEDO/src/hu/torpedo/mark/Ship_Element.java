09031e9e25382d88805e5a7a69793fe50d8cda9b
Pátkai Márk <xdmen6@gmail.com>
2017-10-10 19:11:34 +0200

    Added Mark's Package

---
+++ ThorPEDO/src/hu/torpedo/mark/Ship_Element.java
@@ -0,0 +1,20 @@
+package hu.torpedo.mark;
+
+import java.awt.*;
+
+public class Ship_Element
+{
+    public Color getBase_Color() {
+        return Base_Color;
+    }
+
+    public void setBase_Color(Color base_Color) {
+        Base_Color = base_Color;
+    }
+
+    private Color Base_Color;
+
+
+
+
+}
