f0fd4779639626cc46dda4bb029144077f233329
Zoli <zozo9517@gmail.com>
2017-10-10 19:21:13 +0200

    Grid 2 oldal

--- ThorPEDO/src/Kinezet.java
+++ ThorPEDO/src/Kinezet.java
@@ -9,6 +9,9 @@ public class Kinezet extends Main {
 
 
         obj.setBounds(10,10,1600,768);
+        obj.setLayout(new GridLayout(0,2));
+        obj.add(new JButton("bal oldal"));
+        obj.add(new JButton("jobb oldal"));
         obj.setBackground(Color.DARK_GRAY);
         obj.setResizable(false);
         obj.setVisible(true);
