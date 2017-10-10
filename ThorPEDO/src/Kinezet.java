9f5bdb12d7a31a1eab0088bd9a63b0975c92e7e2
Zoli <zozo9517@gmail.com>
2017-10-10 19:55:51 +0200

    Grid 2 oldal

--- ThorPEDO/src/Kinezet.java
+++ ThorPEDO/src/Kinezet.java
@@ -4,18 +4,27 @@ import javax.swing.*;
 import java.awt.*;
 import static java.awt.Color.*;
 
-public class Kinezet extends Main {
+    public class Kinezet extends Component {
 
     public void ablak(){
         JFrame obj =new JFrame();
 
         obj.setBounds(10,10,1600,768);
         obj.setLayout(new GridLayout(0,2));
-        obj.add(new JButton("bal oldal"));
-        obj.add(new JButton("jobb oldal"));
         obj.setBackground(Color.DARK_GRAY);
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-}
+
+        private static ImageIcon boat;
+        private static int xpos=300;
+        private static int ypos=300;
+
+        public void paint(Graphics g)
+        {
+            boat=new ImageIcon("enemy.png");
+            boat.paintIcon(this,g,xpos,ypos);
+        }
+
+    }
