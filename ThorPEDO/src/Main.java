3b105592ad908f0cb57624cce3d6b47649f593d4
Zoli <zozo9517@gmail.com>
2017-10-10 18:59:05 +0200

    Main

--- ThorPEDO/src/Main.java
+++ ThorPEDO/src/Main.java
@@ -4,16 +4,16 @@ import java.awt.*;
 import static java.awt.Color.*;
 
 public class Main {
-            public static void main(String[] args)
-            {
-                JFrame obj =new JFrame();
+    public static void main(String[] args)
+    {
+        JFrame obj =new JFrame();
 
 
-                obj.setBounds(10,10,1014,768);
-                obj.setBackground(Color.DARK_GRAY);
-                obj.setResizable(false);
-                obj.setVisible(true);
-                obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.setBounds(10,10,1014,768);
+        obj.setBackground(Color.DARK_GRAY);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            }
+    }
 }
