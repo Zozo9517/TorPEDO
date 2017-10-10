bd52ce6e30327a7c6f9baa9aba6aa92cd1c9c0b5
Zoli <zozo9517@gmail.com>
2017-10-10 19:02:55 +0200

    Így lesz jóó!!

--- ThorPEDO/src/Kinezet.java
+++ ThorPEDO/src/Kinezet.java
@@ -1,3 +1,18 @@
-public class Kinezet {
+import javax.swing.*;
+import java.awt.*;
+import static java.awt.Color.*;
+
+public class Kinezet extends Main {
+
+    public void ablak(){
+        JFrame obj =new JFrame();
+
+
+        obj.setBounds(10,10,1014,768);
+        obj.setBackground(Color.DARK_GRAY);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
 }
