18c315c518209c22c93662eddb2bd0a560d8bd19
davy93 <davyzje18@gmail.com>
2017-10-10 18:58:00 +0200

    Sziasztok

--- ThorPEDO/src/Main.java
+++ ThorPEDO/src/Main.java
@@ -1,7 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.Color.*;
+
 public class Main {
+            public static void main(String[] args)
+            {
+                JFrame obj =new JFrame();
 
-        public static void main(String[] args)
-        {
 
-        }
+                obj.setBounds(10,10,1014,768);
+                obj.setBackground(Color.DARK_GRAY);
+                obj.setResizable(false);
+                obj.setVisible(true);
+                obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            }
 }
